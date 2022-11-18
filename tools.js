6f6d2d7eddd567f30f54b17cf895ccc837627687
i2tsuki <mac-daisuki@live.jp>
2022-11-19 06:24:26 +0900

    Implement getActivityIDs()

--- tools.js
+++ tools.js
@@ -2,3 +2,10 @@
 
 // Kudo all displayed activities in your feed
 document.querySelectorAll('[data-testid="kudos_button"]').forEach(function(node) {node.click()});
+
+// Get activities ID in your feed
+function getActivityIDs() {
+    let activities = [];
+    document.querySelectorAll('[data-testid="activity_name"]').forEach(function(node) {activities.push(node.getAttribute("href").replace("/activities/", ""))});
+    console.log(activities);
+}
