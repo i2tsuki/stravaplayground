66e6e23b4c6105770506c409c86e2c399dd7dd9d
i2tsuki <mac-daisuki@live.jp>
2022-11-20 06:22:23 +0900

    Implement removeKudoEntries()

--- tools.js
+++ tools.js
@@ -9,3 +9,15 @@ function getActivityIDs() {
     document.querySelectorAll('[data-testid="activity_name"]').forEach(function(node) {activities.push(node.getAttribute("href").replace("/activities/", ""))});
     console.log(activities);
 }
+
+// Remove activities that you've already kudo
+function removeKudoEntries() {
+    document.querySelectorAll("div[class^='_-_-packages-feed-ui-src-features-FeedEntry__entry-container--']").forEach(function(node) {
+        let kudoButton = node.querySelector('[data-testid="filled_kudos"]');
+        if (kudoButton) {
+         node.remove();   
+        }
+    });
+};
+removeKudoEntries();
+
